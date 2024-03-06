f0828027c7b011e12af5e476eee61ed79f9e3f6b
Kannan <Kannan@LAPTOP-2R5E71P0>
2024-03-06 21:08:35 +0530

    The Local variable

---
+++ reverse/src/reverse/java3.java
@@ -0,0 +1,9 @@
+package reverse;
+import java.io.*;
+ class java3{
+    public static void main(String[] args)
+    {
+        int var = 10;
+        System.out.println("Local Variable: " + var);
+    }
+}
