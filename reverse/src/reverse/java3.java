1a885f977269881542cb6f34f6222850f697390d
Kannan <Kannan@LAPTOP-2R5E71P0>
2024-03-06 21:18:54 +0530

    The local variable changed

--- reverse/src/reverse/java3.java
+++ reverse/src/reverse/java3.java
@@ -3,7 +3,7 @@ import java.io.*;
  class java3{
     public static void main(String[] args)
     {
-        int var = 10;
+        int var = 50;
         System.out.println("Local Variable: " + var);
     }
 }
